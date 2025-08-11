25ac1a4c4b1aec66c5e3af9573714483b2c9d259
kerimbirgi <k.birgi@gmail.com>
2025-08-11 18:33:30 +0100

    update dataset structure

--- etnn/bindingnet/bindingnetcc.py
+++ etnn/bindingnet/bindingnetcc.py
@@ -1,5 +1,7 @@
 # General imports
 from typing import Callable, List, Optional
+import os
+import logging
 import pandas as pd
 from tqdm import tqdm
 
@@ -11,11 +13,8 @@ import torch
 # Class imports
 from etnn.lifter import Lifter, get_adjacency_types, CombinatorialComplexTransform
 from etnn.bindingnet.lifts.registry import LIFTER_REGISTRY
-from preprocess.single_graph_processing import (
-    process_ligand_sdf, 
-    process_protein_pdb_ligand_style, 
-    merge_ligand_and_protein
-)
+
+logger = logging.getLogger(__name__)
 
 
 class BindingNetCC(InMemoryDataset):
@@ -29,7 +28,6 @@ class BindingNetCC(InMemoryDataset):
         connectivity: str,
         connect_cross: bool = False,
         r_cut: float = 5.0,
-        mode: str = 'merged',
         # merge_neighbors: str,
         supercell: Optional[bool] = False,
         transform: Optional[Callable] = None,
@@ -64,22 +62,16 @@ class BindingNetCC(InMemoryDataset):
         super().__init__(
             root, transform, pre_transform, pre_filter, force_reload=force_reload
         )
-        idx = {'ligand': 0, 'protein': 1, 'merged': 2}[mode]
-        self.load(self.processed_paths[idx])
-        # Reload the file that matches the requested mode (default: merged)
-        #loaded = torch.load(self.processed_paths[idx])
-        # `torch.load` may return (data, slices) or (data, slices, *extras*) depending
-        # on the PyG version. We only need the first two elements.
-        #self.data, self.slices = loaded[:2]
+        self.load(self.processed_paths[0])
 
     @property
     def processed_file_names(self) -> list[str]:
-        return ["ligand.pt", "protein.pt", "merged.pt"]
+        return ["merged_lifted.pt"]
 
     def process(self) -> None:
         df = pd.read_csv(self.index)
 
-        ligand_list, protein_list, merged_list = [], [], []
+        merged_list = []
 
         lift = CombinatorialComplexTransform(
             lifter=self.lifter,
@@ -87,45 +79,26 @@ class BindingNetCC(InMemoryDataset):
         )
 
         for index, row in tqdm(df.iterrows(), total=len(df), desc="Processing BindingNetCC"):
-            tuple_id = row['Target ChEMBLID'] + '_' + row['Ligand ChEMBLID']
-            ligand_sdf_path = str(row['ligand_sdf_path'])
-            pocket_pdb_path = str(row['pocket_pdb_path'])
+            tuple_id = row['Target ChEMBLID'] + '_' + row['Molecule ChEMBLID']
+            merged_data_path = os.path.join(self.root, 'merged', f'{tuple_id}.pt')
+            if not os.path.exists(merged_data_path):
+                logger.warning(f"Merged data not found for {tuple_id}")
+                continue
 
-            ligand_data = process_ligand_sdf(ligand_sdf_path) 
-            protein_data = process_protein_pdb_ligand_style(pocket_pdb_path) 
-            merged_data = merge_ligand_and_protein(
-                ligand_data, 
-                protein_data, 
-                connect_cross=self.connect_cross,
-                r_cut=self.r_cut
-            )
-
-            ligand_data.id = tuple_id
-            protein_data.id = tuple_id
-            merged_data.id = tuple_id
-
-            ligand_data = lift(ligand_data)
-            protein_data = lift(protein_data)
+            merged_data = torch.load(merged_data_path)
             merged_data = lift(merged_data)
 
-            if (self.pre_filter is not None and not self.pre_filter(ligand_data)) or \
-                (self.pre_filter is not None and not self.pre_filter(protein_data)) or \
-                (self.pre_filter is not None and not self.pre_filter(merged_data)):
+            y_val = torch.tensor([float(row['-logAffi'])], dtype=torch.float32)
+            merged_data.y = y_val
+
+            if (self.pre_filter is not None and not self.pre_filter(merged_data)):
                 continue
             if self.pre_transform is not None:
-                ligand_data = self.pre_transform(ligand_data)
-                protein_data = self.pre_transform(protein_data)
                 merged_data = self.pre_transform(merged_data)
 
-            ligand_list.append(ligand_data)
-            protein_list.append(protein_data)
             merged_list.append(merged_data)
 
-        for data_list, path in zip(
-            (ligand_list, protein_list, merged_list),
-            self.processed_paths
-        ):
-            self.save(data_list, path)
+        self.save(merged_list, self.processed_paths[0])
 
 
 
